ce8b6062b45d52af5b76f69a3876de1a589b05b5
zapmi <mzapata2991@gmail.com>
2019-04-07 20:03:26 -0500

    some things working but still cant get buttons to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,80 @@
-var number = 100;
+// $(function () {
 
-    var intervalId;
-
-   
-
-    function run() {
-      clearInterval(intervalId);
-      intervalId = setInterval(decrement, 1000);
-    }
-
-    function decrement() {
-
-      number--;
-
-      $("#show-number").html("<h1>Time remianing: " + number + "</h1>");
-
-      if (number === 0) {
-
-        stop();
-
-        alert("Time Up!");
-      }
-    }
-
-    function stop() {
-
-      clearInterval(intervalId);
-    }
-
-    run();
+//Timer
+var number = 5;
+var intervalId;
 
 
-    if (value == "The Lion King"){
-        alert("winner!!");
-    }
+
+function run() {
+  clearInterval(intervalId);
+  intervalId = setInterval(decrement, 1000);
+}
+
+function decrement() {
+  number--;
+
+  $("#showNumber").html("<h1>Time remianing: " + number + "</h1>");
+
+  if (number === 0) {
+    stop();
+    // alert("Time Up!");
+    console.log("times up");
+    $("#form").text("Time's up!!!");
+
+    
+  }
+}
+
+
+// function butClick() {
+//   console.log("times up");
+//   document.getElementById("demo").innerHTML = "Hello World";
+// }
+
+
+
+
+// $("button").click(function() {
+//   $(this).data('clicked', true);
+// });
+
+// if($("button").data('clicked')) {
+//   console.log("times up!!!!");
+//   $("#form").text("Time's up!!!");
+// }
+
+function stop() {
+  clearInterval(intervalId);
+}
+
+run();
+
+
+
+// function submitBut() {
+//   document.getElementById("demo").innerHTML = "Hello World";
+// }
+
+// $("#form").on("submit", function (event) {
+//   event.preventDefault();
+//   stop();
+//   var $answer = $("#form");
+//   var answer = $answer.val();
+//   console.log(answer);
+//   if (answer === "correct"){
+//     $("#form").text("TEST");
+//   }
+// });
+
+
+
+
+
+
+
+
+
+
+
+// });
