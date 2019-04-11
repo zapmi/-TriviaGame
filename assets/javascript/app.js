a9888eee44aa1f6cfa48ec5624aac3c899c5f0f0
zapmi <mzapata2991@gmail.com>
2019-04-10 22:16:05 -0500

    deleted old game with radio inputs and replaced with objects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,34 +4,93 @@
 var number = 5;
 var intervalId;
 
-function run() {
-  clearInterval(intervalId);
-  intervalId = setInterval(decrement, 1000);
-}
+function startGame() {
 
-function decrement() {
-  number--;
-
-  $("#showNumber").html("<h1>Time remianing: " + number + "</h1>");
-
-  if (number === 0) {
-    stop();
-    // alert("Time Up!");
-    console.log("times up");
-    $("#form").text("ALL DONE!! Correct Answers: 0 Incorrect Answers: 0 Unanswered: 0 ");
+  function run() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
   }
+
+  function decrement() {
+    number--;
+
+    $("#showNumber").html("<h1>Time remianing: " + number + "</h1>");
+
+    if (number === 0) {
+      stop();
+      // alert("Time Up!");
+      console.log("times up");
+      $("#quiz").text("ALL DONE!! Correct Answers: " + correct + " Incorrect Answers: " + incorrect + " Unanswered: " + unanswered);
+    }
+  }
+
+  function stop() {
+    clearInterval(intervalId);
+  }
+
+  run();
+
+   var questions = [
+    {
+      question: "What is my favorite color?",
+      choices: ["Red", "Yellow", "Green", "Blue"],
+      answer: "Yellow"
+    },
+    {
+      question: "How many cats do I have?",
+      choices: ["3", "1", "2", "5"],
+      answer: "2"
+    },
+    {
+      question: "What color are my eyes?",
+      choices: ["Hazel", "Brown", "Green", "Blue"],
+      answer: "Hazel"
+    }
+  ]
+
+  var correct = 0;
+  var incorrect = 0;
+  var unanswered = 0;
+
+  for (var i = 0; i < questions.length; i++) {
+    var response = window.prompt(questions[i].prompt);
+    // var response = $("mainQs").append(questions[i]);
+    if (response == questions[i].answer) {
+      correct++;
+      console.log("Correct!");
+    }
+    else if (response !== questions[i].answer) {
+      incorrect++;
+      console.log("Wrong!");
+    }
+    else if (respone == "") {
+
+      unanswered++;
+
+      console.log("blank");
+    }
+  }
+  function doneBut() {
+    $("#quiz").text("ALL DONE!! Correct Answers: " + correct + " Incorrect Answers: " + incorrect + " Unanswered: " + unanswered);
+    stop();
+  } 
+
 }
 
-function stop() {
-  clearInterval(intervalId);
-}
-
-run();
-
-function butClick() {
-  $("#form").text("ALL DONE!! Correct Answers: 0 Incorrect Answers: 0 Unanswered: 0 ");
-  stop();
-}
 
 
-// });
+
+// var wrapper = $('#quiz'), container;
+// for (var question in questions) {
+//   container = $('<div id="quiz1" class="container"></div>');
+//   wrapper.append(container);
+//   container.append('<div class="item">' + question + '</div>');
+//   container.append('<div class="category">' + questions[question].question + '</div>');
+
+// }
+
+
+
+// function myFunction() {
+//   document.getElementById("myDIV").style.display = "none";
+// }
