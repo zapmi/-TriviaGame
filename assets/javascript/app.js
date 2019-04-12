5bd0373d9fba8434dad106f18096e66c940ecaa5
zapmi <mzapata2991@gmail.com>
2019-04-11 23:45:34 -0500

    basically had to start the game all over again

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,12 @@
 // $(function () {
-
-//Timer
-var number = 5;
-var intervalId;
-
-function startGame() {
-
-  function run() {
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-  }
-
-  function decrement() {
-    number--;
-
-    $("#showNumber").html("<h1>Time remianing: " + number + "</h1>");
-
-    if (number === 0) {
-      stop();
-      // alert("Time Up!");
-      console.log("times up");
-      $("#quiz").text("ALL DONE!! Correct Answers: " + correct + " Incorrect Answers: " + incorrect + " Unanswered: " + unanswered);
-    }
-  }
-
-  function stop() {
-    clearInterval(intervalId);
-  }
-
-  run();
-
-   var questions = [
+$(document).ready(function () {
+  //Timer
+  var number = 5;
+  var intervalId;
+  var correct = 0;
+  var incorrect = 0;
+  var unanswered = 0;
+  var questions = [
     {
       question: "What is my favorite color?",
       choices: ["Red", "Yellow", "Green", "Blue"],
@@ -46,51 +22,82 @@ function startGame() {
       choices: ["Hazel", "Brown", "Green", "Blue"],
       answer: "Hazel"
     }
-  ]
+  ];
 
-  var correct = 0;
-  var incorrect = 0;
-  var unanswered = 0;
-
-  for (var i = 0; i < questions.length; i++) {
-    var response = window.prompt(questions[i].prompt);
-    // var response = $("mainQs").append(questions[i]);
-    if (response == questions[i].answer) {
-      correct++;
-      console.log("Correct!");
-    }
-    else if (response !== questions[i].answer) {
-      incorrect++;
-      console.log("Wrong!");
-    }
-    else if (respone == "") {
-
-      unanswered++;
-
-      console.log("blank");
-    }
+  function startGame() {
+    $("#start").hide();
+    $("#showNumber").append();
+    console.log("TEST");
+    // startTime();
+    // displayQuestion();
   }
-  function doneBut() {
-    $("#quiz").text("ALL DONE!! Correct Answers: " + correct + " Incorrect Answers: " + incorrect + " Unanswered: " + unanswered);
-    stop();
-  } 
 
-}
-
-
-
-
-// var wrapper = $('#quiz'), container;
-// for (var question in questions) {
-//   container = $('<div id="quiz1" class="container"></div>');
-//   wrapper.append(container);
-//   container.append('<div class="item">' + question + '</div>');
-//   container.append('<div class="category">' + questions[question].question + '</div>');
-
-// }
+  // Start Game On Click
+  $("#start").on("click", function () {
+    startGame();
+    timer();
+  });
 
 
 
-// function myFunction() {
-//   document.getElementById("myDIV").style.display = "none";
-// }
+  // $('#startBut').click(function () {
+  // $('#quizMessage').hide();
+  // resetVariables();
+  // displayQuestion();
+  // $('#question').show();
+  // $('.btn').show();
+  // $('#timerDisplay').show();
+  // timer.stop();
+  // timer.reset();
+  // timer.start();
+
+  // });
+
+
+  function timer() {
+
+    function run() {
+      clearInterval(intervalId);
+      intervalId = setInterval(decrement, 1000);
+    }
+
+    function decrement() {
+      number--;
+
+      $("#showNumber").html("<h1>Time remianing: " + number + "</h1>");
+
+      if (number === 0) {
+        stop();
+        // alert("Time Up!");
+        console.log("times up");
+        // $("#quiz").text("ALL DONE!! Correct Answers: " + correct + " Incorrect Answers: " + incorrect + " Unanswered: " + unanswered);
+      }
+    }
+
+    function stop() {
+      clearInterval(intervalId);
+    }
+    run();
+  }
+
+  // for (var i = 0; i < questions.length; i++) {
+  //   // var response = window.prompt(questions[i].prompt);
+  //   var response = $("mainQ").append(questions[i]);
+  //   if (response == questions[i].answer) {
+  //     correct++;
+  //     console.log("Correct!");
+  //   }
+  //   else if (response !== questions[i].answer) {
+  //     incorrect++;
+  //     console.log("Wrong!");
+  //   }
+  //   else if (respone == "") {
+
+  //     unanswered++;
+
+  //     console.log("blank");
+  //   }
+  // }
+
+  // }
+});
